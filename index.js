04e4b4ad4f6b3f40dd82865690fbe906cb41cf01
Serhii B <s_blyshchak@ukr.net>
2022-01-02 14:34:28 +0200

    add some lib

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const placeController = require('./controllers/placeController')
+const path = require('path')
 
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 
 
 app.get('/', placeController.getAll )
